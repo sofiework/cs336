2ed599c8eedf7ea4621b83a9aa2046835dcdd9fc
Sofie <sofie.feng.work@gmail.com>
2026-09-02 00:46:12 +0000

    nsys

--- assignment2-systems/memory.sh
+++ assignment2-systems/memory.sh
@@ -6,19 +6,13 @@ set -u
 LABEL=xl
 CTXS=(128 2048)
 MODES=("fwd" "fwd_bwd" "fwd_bwd_opt")
+BATCH=1
 STEPS=3
 OUTDIR=results
 mkdir -p "$OUTDIR"
 
 for ctx in "${CTXS[@]}"; do
   for mode in "${MODES[@]}"; do
-
-    # xl at ctx 2048 does not fit in 80GB with batch 4
-    if [ "$ctx" -ge 2048 ]; then
-      BATCH=1
-    else
-      BATCH=4
-    fi
 
     NAME="mem_${LABEL}_${ctx}_${mode}_bf16"
     echo "=== $NAME ==="
